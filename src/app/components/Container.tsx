29b4072e8f2c66bdf8b50acd8d62052953bc19ad
yuuuuu <sopheak0891@gmail.com>
2025-01-04 14:33:15 +0700

    add audio & Donate Buton

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -1,17 +1,95 @@
 "use client";
 import Image from "next/image";
+import ReactAudioPlayer from "react-audio-player";
 
 const Container = () => {
+  const audioLists = [
+    {
+      name: "Coffee Pouring",
+      src: "coffee-pouring.mp3",
+    },
+    {
+      name: "Coffee Machine",
+      src: "coffee-machine-30756.mp3",
+    },
+    {
+      name: "Coffee Maker",
+      src: "coffee-maker-68090.mp3",
+    },
+    {
+      name: "Customers",
+      src: "coffee-shop-sounds-2-60194.mp3",
+    },
+  ];
+
+  const moreAudioLists = [
+    {
+      name: "Hot Drink",
+      src: "hot-drink-being-poured-and-stirred-40665.mp3",
+    },
+    {
+      name: "Pouring Ice",
+      src: "pouring-ice-25927.mp3",
+    },
+    {
+      name: "Ice & Thunder",
+      src: "pouring-rain-thunder-24236.mp3",
+    },
+
+    {
+      name: "A Cup Of Coffee",
+      src: "stirring-a-cup-of-coffee-193831.mp3",
+    },
+  ];
   return (
-    <div className="flex justify-center py-10 md:flex-row md:justify-start">
-      <div>
-        <Image
-          src="/cafesketch.png"
-          alt="Cafe Sketch"
-          width={400}
-          height={400}
-          className="px-10 w-[400px] h-[400px] md:w-[600px] md:h-[600px] "
-        />
+    <div>
+      <div className="flex flex-col container mx-auto max-w-none items-center justify-center py-10 md:flex-row md:justify-between md:items-start md:px-10">
+        <div>
+          <Image
+            src="/cafesketch.png"
+            alt="Cafe Sketch"
+            width={400}
+            height={400}
+            className="px-10 w-[400px] h-[400px] md:w-[650px] md:h-[500px] "
+          />
+        </div>
+        <div className="pt-10 md:py-0  ">
+          <div className=" flex flex-col items-center ">
+            {audioLists.map((audio, index) => (
+              <div className="py-3" key={index}>
+                <p className=" text-center mb-3">{audio.name}</p>
+                <ReactAudioPlayer
+                  key={index}
+                  src={audio.src}
+                  loop
+                  controls
+                  style={{
+                    borderRadius: "8px",
+                    maxWidth: "170px",
+                  }}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center md:flex-row md:justify-center md:gap-5 ">
+        {moreAudioLists.map((moreAudio, index) => (
+          <div className="py-3 " key={index}>
+            <p className=" text-center mb-3">{moreAudio.name}</p>
+            <ReactAudioPlayer
+              key={index}
+              src={moreAudio.src}
+              loop
+              controls
+              style={{
+                borderRadius: "8px",
+                maxWidth: "170px",
+              }}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ThemeSwitch from "./ThemeSwitch";
-import { PiCoffee } from "react-icons/pi";
+import { PiCoffee, PiFlowerTulipLight } from "react-icons/pi";
 import * as motion from "motion/react-client";
+import Link from "next/link";
+import { CiCoffeeCup } from "react-icons/ci";
 
 const Navbar = () => {
   return (
     <div className="flex-col text-center md:flex md:flex-row-reverse md:justify-between md:items-center md:py-10 md:px-5">
-      <div className="flex justify-center items-center gap-3 text-3xl py-5">
+      <div className="flex justify-center items-center gap-3 text-3xl py-5 ">
         <ThemeSwitch />
         <motion.div
           animate={{ rotate: 360 }}
@@ -14,11 +16,27 @@ const Navbar = () => {
         >
           <PiCoffee />
         </motion.div>
+        <div className=" text-lg bg-amber-800 px-5 py-2 rounded-lg  ">
+          <Link
+            href={
+              "https://paypal.me/chuonsopheaktra?country.x=BH&locale.x=en_US"
+            }
+          >
+            Buy me a coffee{" "}
+            <span className="inline-flex text-3xl items-center align-middle">
+              <CiCoffeeCup />
+            </span>{" "}
+          </Link>
+        </div>
       </div>
       <div>
         <p className="text-2xl px-3 md:text-3xl ">
           Get a cup of coffee <br />
-          and enjoy the moment with me.
+          and enjoy the moment with me{" "}
+          <span className="inline-flex text-3xl items-center align-middle pb-2">
+            {" "}
+            <PiFlowerTulipLight />
+          </span>
         </p>
       </div>
     </div>
